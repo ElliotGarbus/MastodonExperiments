50ea6540bd95d0f6d0fcc46f2e8dedb657969431
ElliotGarbus <elliotg2@cox.net>
2022-11-23 18:09:37 -0700

    timezone realizations!

---
+++ time2datetime.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timezone
+
+t = '2022-11-24T00:05:00.346145Z'
+
+reset_time = datetime.fromisoformat(t)
+print(reset_time)
+last_reset_time = datetime.now(timezone.utc)
+print(f'{last_reset_time.tzinfo=}')
+print(f'{reset_time.tzinfo=}')
+last_reset_time = max(last_reset_time, reset_time)
+print(f'last_reset_time=')
