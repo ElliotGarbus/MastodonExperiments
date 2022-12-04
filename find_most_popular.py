5ce8ac965df5e3b4c725a04c44f06c29372e05d7
ElliotGarbus <elliotg2@cox.net>
2022-12-04 06:33:36 -0700

    opens browser window

--- find_most_popular.py
+++ find_most_popular.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import json
 from pprint import pprint
+import webbrowser
 
 
 users = []
@@ -9,5 +10,15 @@ with open(path) as f:
     for line in f:
         d = json.loads(line)
         users.append({'url': d['url'], 'followers': int(d['followers_count'])})
+top_100 = sorted(users, key=lambda x: x['followers'], reverse=True)[:100]
+pprint(top_100)
 
-pprint(sorted(users, key=lambda x: x['followers'], reverse=True)[:100])
+counter = 0
+for user in top_100:
+    counter += 1
+    webbrowser.open(user['url'])
+    if not (counter % 10):
+        r = input('Continue? [y/n]').lower()
+        if r == 'n':
+            break
+
