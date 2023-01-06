a803dece6073f78876df8e90d9d33b5bdd30ccf9
ElliotGarbus <elliotg2@cox.net>
2023-01-05 20:51:04 -0700

    added cli

--- from_servers.py
+++ from_servers.py
@@ -1,5 +1,5 @@
 # from_servers.py - kick of  a number of processes to pull data from multiple servers
-
+import argparse
 import trio
 from subprocess import DEVNULL
 
@@ -9,12 +9,19 @@ async def launch_process(server, hours):
     await trio.run_process(['python', '.\get_users_async.py', server, '-t', f'{hours}'],
                            shell=True, stdout=DEVNULL)
 
-async def main():
+async def main(duration):
     async with trio.open_nursery() as nursery:
         for s in servers:
-            print(f'launch {s}')
-            nursery.start_soon(launch_process, s, .01)
+            print(f'Directory scan {s} for {duration} hours')
+            nursery.start_soon(launch_process, s, duration)
+    print('All Done!')
 
 if __name__ == '__main__':
-    trio.run(main)
+    description = 'Access multiple servers to access their directory data ' \
+                  'get_users_async.py is used to collect the data '\
+                  'resulting data files are stored in the results directory'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-t', '--time', default=3,  help="execution time in hours, defaults to 3 hours", type=float)
+    args = parser.parse_args()
+    trio.run(main, args.time)
 
