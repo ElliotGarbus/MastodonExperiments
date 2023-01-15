ac6a045ba5294ea946715b9c942a872e2d4e361d
ElliotGarbus <elliotg2@cox.net>
2023-01-15 15:13:12 -0700

    look for errors in all the logs

--- scan_logs.py
+++ scan_logs.py
@@ -4,5 +4,5 @@ p = Path('log')
 for fn in p.glob('*.*'):
     with open(fn) as f:
         for line in f:
-            if 'Response' in line:
+            if 'ERROR:' in line:
                 print(line, end='')
