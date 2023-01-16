f42b7de25efa47607def568fabf3d66e661db7e4
ElliotGarbus <elliotg2@cox.net>
2023-01-15 17:04:41 -0700

    added broken servers

--- from_servers.py
+++ from_servers.py
@@ -8,6 +8,7 @@ import trio
 
 from get_instances import get_instances
 
+
 def batched(iterable, n):
     "Batch data into tuples of length n. The last batch may be shorter."
     # batched('ABCDEFG', 3) --> ABC DEF G
@@ -24,9 +25,10 @@ async def launch_process(server, minutes):
                            shell=True, stdout=DEVNULL)
     print(f'{server} scan complete')
 
+
 async def main(duration):
-    broken_servers = ['loforo.com', 'mas.town', 'mastodon.top']
-    servers = get_instances(300) # 0 for all servers
+    broken_servers = ['loforo.com', 'mas.town', 'mastodon.top', 'meow.social']
+    servers = get_instances(300)  # 0 for all servers
     print(f'{len(servers)} servers selected')
     for s in broken_servers:
         servers.remove(s)
@@ -36,12 +38,12 @@ async def main(duration):
                 nursery.start_soon(launch_process, s, duration)
     print('All Done!')
 
+
 if __name__ == '__main__':
     description = 'Access multiple servers to access their directory data ' \
-                  'get_users_async.py is used to collect the data '\
+                  'get_users_async.py is used to collect the data ' \
                   'resulting data files are stored in the results directory'
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('-t', '--time', default=5,  help="execution time in minutes, defaults to 5 minutes", type=float)
+    parser.add_argument('-t', '--time', default=5, help="execution time in minutes, defaults to 5 minutes", type=float)
     args = parser.parse_args()
     trio.run(main, args.time)
-
