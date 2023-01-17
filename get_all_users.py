446930069f3321aaa1044bcf5e1653c26ae98c40
ElliotGarbus <elliotg2@cox.net>
2023-01-16 17:12:06 -0700

    initial commit - scan all servers in one process

---
+++ get_all_users.py
@@ -0,0 +1,119 @@
+import json
+import logging
+import warnings
+from pathlib import Path
+
+import httpx
+import trio
+from trio import TrioDeprecationWarning
+
+from mastodon_instances_key import mi_info
+
+# turn off deprecation warning issue with a httpx dependency, anyio
+warnings.filterwarnings(action='ignore', category=TrioDeprecationWarning)
+
+"""
+format of mi_info
+mi_info = {'name':'app_name',
+           'app-id':'app_id number',
+           'token': 'token string'}
+"""
+
+
+def get_instances(n):
+    # returns a list of servers, uses instances.social api  https://instances.social/api/doc/
+    header = {'Authorization': 'Bearer ' + mi_info['token']}
+    params = {'count': n, 'include_down': 'false', 'language': 'en', 'sort_by': 'users', 'sort_order': 'desc'}
+    r = httpx.get('https://instances.social/api/1.0/instances/list', headers=header, params=params)
+    d = r.json()
+    return d['instances']  # keys of interest: 'name', 'users', 'active_users'
+
+
+def delete_files(p_dir):
+    # p_dir is a directory Path object
+    for file in p_dir.glob('*.*'):
+        file.unlink()
+
+
+class MastodonInstance:
+    def __init__(self, name, results_dir, log_dir):
+        self.name = name
+        fn = self.name.replace('.', '_') + '.txt'
+        self.data_fn = results_dir / fn
+        log_fn = log_dir / fn
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        file_handler = logging.FileHandler(log_fn)
+        formatter = logging.Formatter('{levelname}:{asctime}:{name}:{message}',
+                                      style='{', datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+        self.params_g = ({'order': 'active', 'local': True, 'limit': 40, 'offset': i} for i in range(0, 100000000, 40))
+        self.unique_url = set()
+        self.finished = False  # used to indicate end of scan, or fatal error
+
+    def save(self, r):
+        print(f'save {self.name}')
+        try:
+            users = r.json()
+        except json.decoder.JSONDecodeError as e:
+            self.logger.error(f'JSON error {e} \n {r.text}')
+            self.finished = True
+            return
+        with open(self.data_fn, 'a') as f:  # only save unique data
+            for user in users:
+                # print(user)
+                if user['url'] in self.unique_url:
+                    continue
+                self.unique_url.add(user['url'])
+                json.dump(user, f)
+                f.write('\n')
+        self.logger.info(f'{len(self.unique_url)} unique records')
+        if not users:
+            self.logger.info('All Data Returned')
+            self.finished = True
+
+    async def get_users(self):
+        url = f"https://{self.name}/api/v1/directory"
+        print(f'Get users from {self.name} ')
+        async with httpx.AsyncClient() as client:
+            while not self.finished:
+                try:
+                    start = trio.current_time()
+                    r = await client.get(url, params=next(self.params_g), timeout=180)  # allow three minutes
+                    r.raise_for_status()
+                    elapsed_time = trio.current_time() - start
+                    if elapsed_time < 1:
+                        await trio.sleep(1 - elapsed_time)  # one second per call rate limit
+                    self.save(r)
+                except httpx.HTTPStatusError as e:
+                    self.logger.error(f'Response {e.response.status_code} while requesting {e.request.url!r}.')
+                    if e.response.status_code != 503:
+                        self.finished = True
+                except (httpx.TimeoutException, httpx.ConnectError, httpx.RemoteProtocolError) as e:
+                    self.logger.error(f'Error {e} on {url}')
+                    self.finished = True
+
+
+async def main():
+    log_dir = Path('log')
+    log_dir.mkdir(exist_ok=True)
+    delete_files(log_dir)
+    results_dir = Path('results')
+    results_dir.mkdir(exist_ok=True)
+    delete_files(results_dir)
+    print('getting instances')
+    instances = get_instances(0)  # 0 is all instances
+    print('instances received')
+    mis = []
+    for instance in instances:
+        mis.append(MastodonInstance(instance['name'], results_dir, log_dir))
+
+    async with trio.open_nursery() as nursery:
+        for mi in mis:
+            nursery.start_soon(mi.get_users)
+    print('Done!')
+
+
+trio.run(main)
