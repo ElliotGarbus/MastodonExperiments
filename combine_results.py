79425ccb8b6ce49059833c836639e4857434c4f6
ElliotGarbus <elliotg2@cox.net>
2023-01-16 17:12:45 -0700

    small utility

---
+++ combine_results.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import json
+from pprint import pprint
+
+all_users = []
+p = Path('results')
+for fn in p.glob('*.*'):
+    with open(fn) as f:
+        for line in f:
+            jl = json.loads(line)
+            all_users.append((jl['url'], jl['followers_count']))
+all_users.sort(key=lambda x: x[1], reverse=True)
+pprint(all_users[:100])
