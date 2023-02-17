473b4aa9ca09e435bdbb5707c64d886f42c4e1cc
ElliotGarbus <elliotg2@cox.net>
2023-02-17 12:15:59 -0700

    Updated filter cases

--- clean_zero_peers.py
+++ clean_zero_peers.py
@@ -5,7 +5,16 @@ print('data read')
 clean = [p for p in set(peers) if not any([p.endswith('.cispa.saarland\n'),
                                            p.endswith('.ngrok.io\n'),
                                            p.endswith('.local\n'),
+                                           p.endswith('activitypub-troll.cf\n'),
+                                           p.endswith('misskey-forkbomb.cf\n'),
+                                           p.endswith('repl.co\n'),
+                                           p.endswith('gab.best\n'),
+                                           p.startswith("192."),
                                            ':' in p,
+                                           '..' in p,
+                                           '.' not in p,
+                                           len(p.split('.')[0]) >= 40,
+                                           p.split('.')[0].isupper(),
                                            ])]
 print('Cleaned')
 clean.sort()
