ae1e4c984c8684398400ed75731f9700dd3be3ff
ElliotGarbus <elliotg2@cox.net>
2023-02-23 05:09:47 -0700

    added keepawake

--- run_all.py
+++ run_all.py
@@ -7,30 +7,33 @@ run_all.py run all phases to collect all the mastodon user data
 from datetime import datetime
 
 import trio
+from wakepy import keepawake
 
 import crawl_instances
 import get_instance_info
 import get_all_users
 
-start = datetime.now()
-trio.run(crawl_instances.main)
-end = datetime.now()
-crawl_time = end - start
+with keepawake(keep_screen_awake=False):
+    start = datetime.now()
+    trio.run(crawl_instances.main)
+    end = datetime.now()
+    crawl_time = end - start
 
-start = datetime.now()
-trio.run(get_instance_info.main)
-end = datetime.now()
-get_info_time = end - start
+    start = datetime.now()
+    trio.run(get_instance_info.main)
+    end = datetime.now()
+    get_info_time = end - start
 
-tart = datetime.now()
-trio.run(get_all_users.main)
-end = datetime.now()
-get_users_time = end - start
+    tart = datetime.now()
+    trio.run(get_all_users.main)
+    end = datetime.now()
+    get_users_time = end - start
 
-print(f'Time to crawl all instances: {crawl_time}')
-print(f'Time to get instance info: {get_info_time}')
-print(f'Time to get all users: {get_users_time}')
-print(f'Total execution time: {crawl_time + get_info_time + get_users_time}')
-print('mastodon_instances.txt - List of all servers')
-print('instance_info.txt - info records for all servers')
-print('results directory - contains files with all users per server')
+    print('=' * 80)
+    print(f'Time to crawl all instances: {crawl_time}')
+    print(f'Time to get instance info: {get_info_time}')
+    print(f'Time to get all users: {get_users_time}')
+    print(f'Total execution time: {crawl_time + get_info_time + get_users_time}')
+    print('mastodon_instances.txt - List of all servers')
+    print('instance_info.txt - info records for all servers')
+    print('results directory - contains files with all users per server')
