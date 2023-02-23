03d0802e78859099e8f83541766c8d667fbfa304
ElliotGarbus <elliotg2@cox.net>
2023-02-22 21:30:39 -0700

    update error condition

--- scan_logs.py
+++ scan_logs.py
@@ -4,5 +4,5 @@ p = Path('log')
 for fn in p.glob('*.*'):
     with open(fn) as f:
         for line in f:
-            if 'codepoint' in line:
+            if 'TypeError' in line:
                 print(line)
