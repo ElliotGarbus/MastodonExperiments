4799ae102389eb8b3dc88f29c460a3382908e8ca
ElliotGarbus <elliotg2@cox.net>
2023-02-23 19:03:06 -0700

    looping over phases

--- run_all.py
+++ run_all.py
@@ -1,8 +1,8 @@
 """
 run_all.py run all phases to collect all the mastodon user data
 1) run crawl_instances.py, create mastodon_instances.txt
-2) run get_intantance_info.py, create instanance_info.txt, contains the instance records for all the instances
-3) run get_all_users, creates the resultes directory, with the users of each instance
+2) run get_instance_info.py, create instance_info.txt, contains the instance records for all the instances
+3) run get_all_users, creates the "results" directory, with the users of each instance
 """
 from datetime import datetime
 
@@ -14,30 +14,23 @@ import get_instance_info
 import get_all_users
 
 with keepawake(keep_screen_awake=False):
-    start = datetime.now()
-    trio.run(crawl_instances.main)
-    end = datetime.now()
-    crawl_time = end - start
+    phases = ({'function': crawl_instances.main, 'timer_key': 'crawl_time'},
+              {'function': get_instance_info.main, 'timer_key': 'get_info_time'},
+              {'function': get_all_users.main, 'timer_key': 'get_users_time'})
+    timer = {}
 
-    start = datetime.now()
-    trio.run(get_instance_info.main)
-    end = datetime.now()
-    get_info_time = end - start
-
-    tart = datetime.now()
-    trio.run(get_all_users.main)
-    end = datetime.now()
-    get_users_time = end - start
+    for phase in phases:
+        start = datetime.now()
+        trio.run(phase['function'])
+        end = datetime.now()
+        timer[phase['timer_key']] = end - start
 
     print('=' * 80)
-    print(f'Time to crawl all instances: {crawl_time}')
-    print(f'Time to get instance info: {get_info_time}')
-    print(f'Time to get all users: {get_users_time}')
-    print(f'Total execution time: {crawl_time + get_info_time + get_users_time}')
+    print(f'Time to crawl all instances: {timer["crawl_time"]}')
+    print(f'Time to get instance info: {timer["get_info_time"]}')
+    print(f'Time to get all users: {timer["get_users_time"]}')
+    print(f'Total execution time: {sum(timer.values())}')
     print('mastodon_instances.txt - List of all servers')
     print('instance_info.txt - info records for all servers')
     print('results directory - contains files with all users per server')
-
-# todo: move timing to decorator?
-# create option to ignore emoji servers in crawl
-# make using the zero_peers file an option in crawl
+    print('=' * 80)
