bc564f014a2e5552d142440c6255f0f1569c1875
ElliotGarbus <elliotg2@cox.net>
2023-02-23 21:57:51 -0700

    changed time formatting

--- run_all.py
+++ run_all.py
@@ -4,7 +4,7 @@ run_all.py run all phases to collect all the mastodon user data
 2) run get_instance_info.py, create instance_info.txt, contains the instance records for all the instances
 3) run get_all_users, creates the "results" directory, with the users of each instance
 """
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import trio
 from wakepy import keepawake
@@ -25,11 +25,17 @@ with keepawake(keep_screen_awake=False):
         end = datetime.now()
         timer[phase['timer_key']] = end - start
 
+    total_time = sum(timer.values(), start=timedelta())
+    # convert to str, drop microseconds
+    total_time = str(total_time).split('.')[0]
+    for k in timer:
+        timer[k] = str(timer[k]).split('.')[0]
+
     print('=' * 80)
     print(f'Time to crawl all instances: {timer["crawl_time"]}')
     print(f'Time to get instance info: {timer["get_info_time"]}')
     print(f'Time to get all users: {timer["get_users_time"]}')
-    print(f'Total execution time: {sum(timer.values())}')
+    print(f'Total execution time: {total_time}')
     print('mastodon_instances.txt - List of all servers')
     print('instance_info.txt - info records for all servers')
     print('results directory - contains files with all users per server')
