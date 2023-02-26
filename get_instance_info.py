5c77590fee39086bc050ab0125af678df29c9116
ElliotGarbus <elliotg2@cox.net>
2023-02-26 13:36:41 -0700

    v1 API completed

--- get_instance_info.py
+++ get_instance_info.py
@@ -11,6 +11,7 @@ from idna.core import InvalidCodepoint
 # turn off deprecation warning issue with a httpx dependency, anyio
 warnings.filterwarnings(action='ignore', category=trio.TrioDeprecationWarning)
 
+INSTANCE_API_VERSION = 'v1'
 
 def get_info_sync(url):
     """
@@ -46,7 +47,7 @@ def get_info_sync(url):
 
 async def get_info(name):
     print(f'Get info from {name} ')
-    url = f"https://{name}/api/v2/instance"
+    url = f"https://{name}/api/{INSTANCE_API_VERSION}/instance"
     # properly encode urls that have emoji characters or other unicode
     async with httpx.AsyncClient() as client:
         try:
@@ -59,7 +60,7 @@ async def get_info(name):
         except httpx.HTTPStatusError as e:
             logging.error(f'Response {e.response.status_code} while requesting {e.request.url!r}.')
             return None
-        except (httpx.RequestError) as e:
+        except httpx.RequestError as e:
             logging.error(f'Connection Error {e} on {url}')
             return None
         except InvalidCodepoint as e:
@@ -72,11 +73,46 @@ async def get_info(name):
             raise e
 
 
+def is_info_valid(info):
+    """
+    Tests if the info data returned from a server is valid
+    :param info: The info dict from a server
+    :return: True if the data is valid
+    """
+    if INSTANCE_API_VERSION == 'v1':
+        valid = (info and
+                 'stats' in info and
+                 isinstance(info['stats']['user_count'], int) and
+                 info['stats']['user_count'] and
+                 isinstance(info['uri'], str))
+    elif INSTANCE_API_VERSION == 'v2':
+        valid = (info and 'domain' in info)
+    else:
+        raise ValueError(f'Invalid value {INSTANCE_API_VERSION} for INSTANCE_API_VERSION')
+    return valid
+
+
+def clean_uri(info):
+    """
+    removes the leading https:// for the uri field, only a small number of servers include this prefix
+    makes the uri data consistent across all servers
+    :param info: The info dict from a server
+    :return: None, modifies data in place
+    """
+    if INSTANCE_API_VERSION == 'v1':
+        info['uri'] = info['uri'].replace('https://', '')
+    elif INSTANCE_API_VERSION == 'v2':
+        pass
+    else:
+        raise ValueError(f'Invalid value {INSTANCE_API_VERSION} for INSTANCE_API_VERSION')
+
+
 async def get_info_task(instances, outfile):
     while instances:
         name = instances.pop()
         info = await get_info(name)
-        if info and 'domain' in info:  # domain field indicating a valid response
+        if is_info_valid(info):
+            clean_uri(info)
             with open(outfile, 'a') as f:
                 json.dump(info, f)
                 f.write('\n')
