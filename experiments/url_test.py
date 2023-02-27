da4f57d4df191e17bdfff1cc364a10fedefecd6e
ElliotGarbus <elliotg2@cox.net>
2023-02-27 12:37:05 -0700

    env test

--- experiments/url_test.py
+++ experiments/url_test.py
@@ -2,6 +2,14 @@ from urllib.parse import urlparse, urlunparse
 import requests
 import httpx
 from pprint import pprint
+import os
+
+env = os.getenv('USERAGENT')
+if env:
+    print(f'user_agent = {env}')
+    headers = {'user-agent': env}
+else:
+    print('default user agent')
 
 def convert_idna_address(url: str) -> str:
     parsed_url = urlparse(url)
@@ -13,17 +21,18 @@ def convert_idna_address(url: str) -> str:
 # url = 'https://🍺🌯.to/api/v1/instance/peers'
 # url = 'https://sleeping.town/api/v1/instance/peers'
 url = 'https://az.social/api/v1/instance'
-headers = {'user-agent': 'simple-test'}
+
+
 
 # converted_url = convert_idna_address(url)
 # print(f'{url=} {converted_url=}')
 
 r = requests.get(url, headers=headers, timeout=3.5)
 out = r.json()
-print(f'requests: {out}')
+# print(f'requests: {out}')
 print(r.request.headers)
 
 r = httpx.get(url, headers=headers, timeout=3.5)
 out = r.json()
-print(f'httpx: {out}')
+# print(f'httpx: {out}')
 print(r.request.headers)
