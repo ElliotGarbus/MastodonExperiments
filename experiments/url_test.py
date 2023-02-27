d15b9c932644ff5df174e7685ff53a6966f01512
ElliotGarbus <elliotg2@cox.net>
2023-02-27 08:10:46 -0700

    mod user-agent

--- experiments/url_test.py
+++ experiments/url_test.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse, urlunparse
 import requests
 import httpx
+from pprint import pprint
 
 def convert_idna_address(url: str) -> str:
     parsed_url = urlparse(url)
@@ -10,14 +11,18 @@ def convert_idna_address(url: str) -> str:
 
 
 # url = 'https://🍺🌯.to/api/v1/instance/peers'
-url = 'https://sleeping.town/api/v1/instance/peers'
-converted_url = convert_idna_address(url)
-print(f'{url=} {converted_url=}')
+# url = 'https://sleeping.town/api/v1/instance/peers'
+url = 'https://az.social/api/v1/instance'
+headers = {'user-agent': 'simple-test'}
 
-r = requests.get(converted_url, timeout=(3.5, 3.5))
+# converted_url = convert_idna_address(url)
+# print(f'{url=} {converted_url=}')
+
+r = requests.get(url, headers=headers, timeout=3.5)
 out = r.json()
 print(f'requests: {out}')
+print(r.request.headers)
 
-# r = httpx.get(converted_url, timeout=3.5)
-# out = r.json()
-# print(f'httpx: {out}')
+r = httpx.get(url, timeout=3.5)
+out = r.json()
+print(f'httpx: {out}')
