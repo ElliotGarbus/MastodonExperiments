8a5246ce5b247a4046a174757632bfcf88be3d88
ElliotGarbus <elliotg2@cox.net>
2023-02-28 17:46:42 -0700

    user_agent sent from env var

--- flags.py
+++ flags.py
@@ -1,10 +1,11 @@
 """
 Constants that control rarely changed functionality, and the user-agent header
 """
+import os
 
 INSTANCE_API_VERSION = 'v1'  # select v1 or v2 of the api, will change when v1 is no longer supported
 IGNORE_EMOJI_URL = True  # used to ignore the emoji urls...
-CREATE_GRAPH = False   # create a graph file of the peer server data
+CREATE_GRAPH = False  # create a graph file of the peer server data
 
 try:
     user_agent_header = {'user-agent': os.environ['USERAGENT']}
