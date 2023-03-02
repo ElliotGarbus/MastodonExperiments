c7fc55b824385796170043706b264f909e263629
ElliotGarbus <elliotg2@cox.net>
2023-03-01 17:02:07 -0700

    consloidated output files

--- px_get_mastodon_data.py
+++ px_get_mastodon_data.py
@@ -4,8 +4,10 @@ px_get_mastodon_data.py run all phases to collect all the mastodon user data
 2) run p2_get_instance_info.py, create instance_info.txt, contains the instance records for all the instances
 3) run p3_get__users, creates the "results" directory, with the users of each instance
 """
-from datetime import datetime, timedelta
+
 import argparse
+from datetime import datetime, timedelta
+from pathlib import Path
 
 import trio
 from wakepy import keepawake
@@ -14,14 +16,16 @@ from p1_get_instances import get_instances
 from p2_get_instance_info import get_instance_info
 from p3_get_users import get_users
 
+
 def cli():
     description = 'Get the mastodon instance info and optionally user data.'
-    mode_help = '"info" collects the instance info for all servers, "users" collects the info and user data. '\
+    mode_help = '"info" collects the instance info for all servers, "users" collects the info and user data. ' \
                 'Defaults to "users"'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('-m', '--mode', default='users', help=mode_help, choices=['info', 'users'])
     args = parser.parse_args()
     return args.mode
+
 
 def print_execution_times(t):
     # t is a dict that holds the execution times per phase
@@ -43,13 +47,20 @@ def print_execution_times(t):
     print('results directory - contains files with all users per server')
     print('=' * 80)
 
+
 def consolidate_results(m):
     # m is the execution mode either 'info' or 'users' from the cli
-    # todo: create consolidated result
     print('Consolidating results...', end='')
-
+    files = Path('results').glob('*.*')
+    with open('consolidated_output.txt', 'w') as outfile:
+        with open('instance_info.txt') as info_file:
+            outfile.write(info_file.read())
+        if m == 'users':
+            for file in files:
+                with open(file) as f:
+                    outfile.write(f.read())
     print('Done!')
-    pass
+
 
 phases = ({'function': get_instances, 'timer_key': 'get_instances_time'},
           {'function': get_instance_info, 'timer_key': 'get_info_time'},
@@ -69,6 +80,3 @@ with keepawake(keep_screen_awake=False):
 
 print_execution_times(timer)
 consolidate_results(mode)
-
-
-
