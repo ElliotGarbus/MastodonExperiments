e2bc9b6beef14eda3a78eec590edf457c192fb6a
ElliotGarbus <elliotg2@cox.net>
2023-03-02 21:01:45 -0700

    initial - parsing robot.txt

--- experiments/robots_play.py
+++ experiments/robots_play.py
@@ -1,4 +1,5 @@
 from urllib.robotparser import RobotFileParser
+from urllib.parse import urljoin
 import json
 
 import requests
@@ -6,26 +7,24 @@ import requests
 rp = RobotFileParser()
 rp.set_url('https://az.social/robots.txt')
 rp.read()
-print(f'{rp.can_fetch("*","https://az.social/api")=}')
-# print(f'{rp.crawl_delay("*")=}')
-# print(f'{rp.mtime()=}')
+print(f'{rp.can_fetch("*","/api")=}')
+print(f'{rp.can_fetch("my crawler", "/media_proxy/")=}')
 
 # with open('../seed_instances.json') as f:
 #     sites = json.load(f)
 
-sites = ["mastodon.social", "mastodon.world", "mas.to", "mastodon.lol", "techhub.social", "universeodon.com"]
+sites = ["mastodon.social", "mastodon.world", "mas.to", "mastodon.lol", "techhub.social",
+         "universeodon.com", 'www.musi-cal.com']
 
 for site in sites:
-    robot_url = f'https://{site}/robots.txt'
+    robot_url = f'https://{site}'
+    rp = RobotFileParser()
     rp.set_url(robot_url)
     rp.read()
-    url = f"https://{site}"
-    print(f' {url} {rp.can_fetch("*", url)=}')
+    print(f'{site} {rp.can_fetch("my crawler", "https://{site}/api/")=}')
+    print(f'{site} {rp.can_fetch("my crawler", "https://{site}/media_proxy/")=}')
 
-for site in sites:
-    robot_url = f'https://{site}/robots.txt'
-    r = requests.get(robot_url)
-    print(robot_url)
-    print(r.text)
+
+
 
 
