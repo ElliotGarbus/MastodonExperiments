782e90732267f83d22880dd680718ff46456cad4
ElliotGarbus <elliotg2@cox.net>
2023-03-27 05:55:31 -0700

    example of accessing the ip address

--- experiments/get with ip.py
+++ experiments/get with ip.py
@@ -4,6 +4,6 @@ with httpx.Client() as client:
     with client.stream("GET", "https://httpbin.org/get") as response:
         network_stream = response.extensions["network_stream"]
         server_addr = network_stream.get_extra_info("server_addr")
-        # print(response.json())
+        #  print(response.json())
         print(response.read().decode())
         print(server_addr)
