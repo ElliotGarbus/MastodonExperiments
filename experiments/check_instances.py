ee8ea0b803bd4bdfa14b239e794adbcf79664c6c
ElliotGarbus <elliotg2@cox.net>
2023-04-02 09:15:44 -0700

    test for duplicates

--- experiments/check_instances.py
+++ experiments/check_instances.py
@@ -1,16 +1,24 @@
 import json
 
 instances = []
+lines = []
 with open('../instance_info.txt') as f:
     for line in f:
         d = json.loads(line)
         instances.append(d['uri'])
-in_set = set(instances)
-print(f'{len(instances)=} {len(in_set)=}')
-instances = list(set(instances))  # remove duplicates
-instances.sort()
-for i in instances:
-    print(i)
+        lines.append(line)
+# in_set = set(instances)
+# print(f'{len(instances)=} {len(in_set)=}')
+# instances = list(set(instances))  # remove duplicates
+# instances.sort()
+# for i in instances:
+#     print(i)
+lines.sort()
+print(f'{len(lines)=} {len(set(lines))=}')
+for l0, l1 in zip(lines[::2], lines[1::2]):
+    if l0 == l1:
+        print(l0)
+
 
 
 
